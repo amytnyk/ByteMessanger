af9abbe3bd81576750fab855710f6a4d473b0511
amytnyk <alex.mytnyk@ukr.net>
2018-04-28 15:36:17 +0300

    alpha

--- app/src/main/java/alexm/bytemessanger/activities/FindActivity.java
+++ app/src/main/java/alexm/bytemessanger/activities/FindActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import alexm.bytemessanger.R;
 
-public class NewServerActivity extends AppCompatActivity {
+public class FindActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_server);
+        setContentView(R.layout.activity_find);
     }
 }
